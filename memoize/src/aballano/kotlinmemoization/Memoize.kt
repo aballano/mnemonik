34f3964afa4cc34a80cf2fe67a2625413d98c0ae
Krzysztof Sroga <krzysztof@sroga.pw>
2019-03-30 14:07:58 +0200

    updated kotlin, add memoizeSuspend

--- memoize/src/aballano/kotlinmemoization/Memoize.kt
+++ memoize/src/aballano/kotlinmemoization/Memoize.kt
@@ -23,34 +23,69 @@ const val DEFAULT_CAPACITY = 1 shl 8
 fun <A, R> ((A) -> R).memoize(initialCapacity: Int = DEFAULT_CAPACITY): (A) -> R {
     val cache: MutableMap<A, R> = HashMap(initialCapacity)
     return { a: A ->
-        cache.getOrPut(a, { this(a) })
+        cache.getOrPut(a) { this(a) }
     }
 }
 
 fun <A, B, R> ((A, B) -> R).memoize(initialCapacity: Int = DEFAULT_CAPACITY): (A, B) -> R {
     val cache: MutableMap<Pair<A, B>, R> = HashMap(initialCapacity)
     return { a: A, b: B ->
-        cache.getOrPut(a to b, { this(a, b) })
+        cache.getOrPut(a to b) { this(a, b) }
     }
 }
 
 fun <A, B, C, R> ((A, B, C) -> R).memoize(initialCapacity: Int = DEFAULT_CAPACITY): (A, B, C) -> R {
     val cache: MutableMap<Triple<A, B, C>, R> = HashMap(initialCapacity)
     return { a: A, b: B, c: C ->
-        cache.getOrPut(Triple(a, b, c), { this(a, b, c) })
+        cache.getOrPut(Triple(a, b, c)) { this(a, b, c) }
     }
 }
 
 fun <A, B, C, D, R> ((A, B, C, D) -> R).memoize(initialCapacity: Int = DEFAULT_CAPACITY): (A, B, C, D) -> R {
     val cache: MutableMap<Quadruple<A, B, C, D>, R> = HashMap(initialCapacity)
     return { a: A, b: B, c: C, d: D ->
-        cache.getOrPut(Quadruple(a, b, c, d), { this(a, b, c, d) })
+        cache.getOrPut(Quadruple(a, b, c, d)) { this(a, b, c, d) }
     }
 }
 
 fun <A, B, C, D, E, R> ((A, B, C, D, E) -> R).memoize(initialCapacity: Int = DEFAULT_CAPACITY): (A, B, C, D, E) -> R {
     val cache: MutableMap<Quintuple<A, B, C, D, E>, R> = HashMap(initialCapacity)
     return { a: A, b: B, c: C, d: D, e: E ->
-        cache.getOrPut(Quintuple(a, b, c, d, e), { this(a, b, c, d, e) })
+        cache.getOrPut(Quintuple(a, b, c, d, e)) { this(a, b, c, d, e) }
+    }
+}
+
+fun <A, R> (suspend (A) -> R).memoizeSuspend(initialCapacity: Int = DEFAULT_CAPACITY): suspend (A) -> R {
+    val cache: MutableMap<A, R> = HashMap(initialCapacity)
+    return { a: A ->
+        cache.getOrPut(a) { this(a) }
+    }
+}
+
+fun <A, B, R> (suspend (A, B) -> R).memoizeSuspend(initialCapacity: Int = DEFAULT_CAPACITY): suspend (A, B) -> R {
+    val cache: MutableMap<Pair<A, B>, R> = HashMap(initialCapacity)
+    return { a: A, b: B ->
+        cache.getOrPut(a to b) { this(a, b) }
+    }
+}
+
+fun <A, B, C, R> (suspend (A, B, C) -> R).memoizeSuspend(initialCapacity: Int = DEFAULT_CAPACITY): suspend (A, B, C) -> R {
+    val cache: MutableMap<Triple<A, B, C>, R> = HashMap(initialCapacity)
+    return { a: A, b: B, c: C ->
+        cache.getOrPut(Triple(a, b, c)) { this(a, b, c) }
+    }
+}
+
+fun <A, B, C, D, R> (suspend (A, B, C, D) -> R).memoizeSuspend(initialCapacity: Int = DEFAULT_CAPACITY): suspend (A, B, C, D) -> R {
+    val cache: MutableMap<Quadruple<A, B, C, D>, R> = HashMap(initialCapacity)
+    return { a: A, b: B, c: C, d: D ->
+        cache.getOrPut(Quadruple(a, b, c, d)) { this(a, b, c, d) }
+    }
+}
+
+fun <A, B, C, D, E, R> (suspend (A, B, C, D, E) -> R).memoizeSuspend(initialCapacity: Int = DEFAULT_CAPACITY): suspend (A, B, C, D, E) -> R {
+    val cache: MutableMap<Quintuple<A, B, C, D, E>, R> = HashMap(initialCapacity)
+    return { a: A, b: B, c: C, d: D, e: E ->
+        cache.getOrPut(Quintuple(a, b, c, d, e)) { this(a, b, c, d, e) }
     }
 }
