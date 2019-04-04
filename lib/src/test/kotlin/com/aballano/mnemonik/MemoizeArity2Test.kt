d2257d94f73b7aa1c55cb5ae1353bfbb6aa8132b
Alberto Ballano <alberto.ballano@xing.com>
2019-04-04 11:52:21 +0200

    Added a test for each function arity.

--- lib/src/test/kotlin/com/aballano/mnemonik/MemoizeArity2Test.kt
+++ lib/src/test/kotlin/com/aballano/mnemonik/MemoizeArity2Test.kt
@@ -5,7 +5,7 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import java.util.concurrent.atomic.AtomicInteger
 
-class MemoizeTest : StringSpec() {
+class MemoizeArity2Test : StringSpec() {
 
     private val counter = AtomicInteger(0)
 
@@ -43,26 +43,16 @@ class MemoizeTest : StringSpec() {
 
         "memoized function should call every time with different params" {
             assertAll { a: String, b: String, c: String, d: String ->
-                givenAllClear {
+                if (notEqual(a, b, c, d)) givenAllClear {
                     val concatSpacedMemoized = givenMemoizedFunction()
                     concatSpacedMemoized(a, b) shouldBe "$a $b"
                     concatSpacedMemoized(b, c) shouldBe "$b $c"
-                    concatSpacedMemoized(a, d) shouldBe "$a $d"
-                    counter.get() shouldBe 3
-                }
-            }
-        }
-
-        "memoized function should call only every time with switched params" {
-            assertAll { a: String, b: String ->
-                givenAllClear {
-                    val concatSpacedMemoized = givenMemoizedFunction()
-                    concatSpacedMemoized(a, b) shouldBe "$a $b"
                     concatSpacedMemoized(b, a) shouldBe "$b $a"
-                    counter.get() shouldBe 2
+                    concatSpacedMemoized(a, d) shouldBe "$a $d"
+                    concatSpacedMemoized(a, b) shouldBe "$a $b"
+                    counter.get() shouldBe 4
                 }
             }
         }
-
     }
 }
